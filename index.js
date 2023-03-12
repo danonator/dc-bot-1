923087bfa817a1236887dfc2ea3e787fe73b66e7
Dane Ohr <danonator821@gmail.com>
2023-03-12 17:54:00 -0400

    Added Necessary Files

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ const client = new Discord.Client ({
   intents: ['GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILDS']
 })
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+let bot = {
+  client,
+  prefix: "n.",
+  owners: ["719220179502104626"]
+}
 
-client.on('messageCreate', (message) => {
-  if(message.content.toUpperCase() == 'HI' || message.content.toUpperCase() == 'HELLO'){
-    message.reply(`Hey there, ${message.author.username}!`);
-    message.react('👋');
-  }
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+
+module.exports = bot
+
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}`)
+// })
+
+// client.on('messageCreate', (message) => {
+//   if(message.content.toUpperCase() == 'HI' || message.content.toUpperCase() == 'HELLO'){
+//     message.reply(`Hey there, ${message.author.username}!`);
+//     message.react('👋');
+//   }
+// })
 
 client.login(process.env.TOKEN);
